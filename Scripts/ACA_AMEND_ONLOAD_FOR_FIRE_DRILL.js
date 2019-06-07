0ea2d4189b9e16f67752722cb0e22884f4313ee5
lluaca <44002415+lluaca@users.noreply.github.com>
2019-06-07 14:10:28 -0700

    Update ACA_AMEND_ONLOAD_FOR_FIRE_DRILL.js

--- Scripts/ACA_AMEND_ONLOAD_FOR_FIRE_DRILL.js
+++ Scripts/ACA_AMEND_ONLOAD_FOR_FIRE_DRILL.js
@@ -1,30 +1,124 @@
 /*------------------------------------------------------------------------------------------------------/
-| Program : ACA_AMEND_ONLOAD_FOR_FIRE_DRILLS.js
-| Event   : ACA Amend OnLoad
+| Program : ACA Page Flow Template.js
+| Event   : ACA Page Flow Template
 |
-| Usage   : Accela Master Script ACA_Amend_Onload.js customized to only copy CAP Detail and People information.
+| Usage   : Master Script by Accela.  See accompanying documentation and release notes.
 |
-| Client  : Loma Linda University Hospital
+| Client  : N/A
 | Action# : N/A
 |
-| Notes   : This is used to copy information from the parent Department record.
+| Notes   :
 |
 /------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------/
+| START User Configurable Parameters
+|
+|     Only variables in the following section may be changed.  If any other section is modified, this
+|     will no longer be considered a "Master" script and will not be supported in future releases.  If
+|     changes are made, please add notes above.
+/------------------------------------------------------------------------------------------------------*/
+var showMessage = false; // Set to true to see results in popup window
+var showDebug = false; // Set to true to see debug messages in popup window
+var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
+var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
+var cancel = false;
+var useCustomScriptFile = true;  			// if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
+/*------------------------------------------------------------------------------------------------------/
+| END User Configurable Parameters
+/------------------------------------------------------------------------------------------------------*/
+var startDate = new Date();
+var startTime = startDate.getTime();
+var message = ""; // Message String
+var debug = ""; // Debug String
+var br = "<BR>"; // Break Tag
 
- 
-var br = "<BR>";
- 
-var errorMessage = "";
- 
-var errorCode = "0";
-var currentUserID = aa.env.getValue("CurrentUserID");
-if (currentUserID.indexOf("PUBLICUSER") == 0) { currentUserID = "ADMIN" ; publicUser = true }  // ignore public users
+var useSA = false;
+var SA = null;
+var SAScript = null;
+var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_FOR_EMSE");
+if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
+	useSA = true;
+	SA = bzr.getOutput().getDescription();
+	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS", "SUPER_AGENCY_INCLUDE_SCRIPT");
+	if (bzr.getSuccess()) {
+		SAScript = bzr.getOutput().getDescription();
+	}
+}
 
-//execute
-var capModelInited = aa.env.getValue("CAP_MODEL_INITED");
-if (capModelInited != "TRUE")
-{
-    copy();
+if (SA) {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA));
+	eval(getScriptText(SAScript, SA));
+} else {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+}
+
+eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));
+
+
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		if (useProductScripts) {
+			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+
+
+var cap = aa.env.getValue("CapModel");
+var parentId = cap.getOutput().getParentCapID();
+
+// page flow custom code begin
+
+try {
+
+  var br = "<BR>";
+   
+  var errorMessage = "";
+   
+  var errorCode = "0";
+  var currentUserID = aa.env.getValue("CurrentUserID");
+  if (currentUserID.indexOf("PUBLICUSER") == 0) { currentUserID = "ADMIN" ; publicUser = true }  // ignore public users
+
+  //execute
+  var capModelInited = aa.env.getValue("CAP_MODEL_INITED");
+  if (capModelInited != "TRUE")
+  {
+      copy();
+  }
+
+} catch (err) {
+	handleError(err, "Page Flow Script");
+}
+
+
+// page flow custom code end
+
+
+if (debug.indexOf("**ERROR") > 0) {
+	aa.env.setValue("ErrorCode", "1");
+	aa.env.setValue("ErrorMessage", debug);
+} else {
+	if (cancel) {
+		aa.env.setValue("ErrorCode", "-2");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	} else {
+		aa.env.setValue("ErrorCode", "0");
+		if (showMessage)
+			aa.env.setValue("ErrorMessage", message);
+		if (showDebug)
+			aa.env.setValue("ErrorMessage", debug);
+	}
 }
 
 /*------------------------------------------------------------------------------------------------------/
@@ -91,13 +185,12 @@ function copy()
       
 		// copyCapWorkDesInfo(parentCapId, targetCapId);
 	  
-		// var amendCapModel = aa.cap.getCapViewBySingle4ACA(targetCapId);
-    // amendCapModel.getCapType().setSpecInfoCode(capModel.getCapType().getSpecInfoCode()); 
+		var amendCapModel = aa.cap.getCapViewBySingle4ACA(targetCapId);
+    amendCapModel.getCapType().setSpecInfoCode(capModel.getCapType().getSpecInfoCode()); 
 
-		// aa.env.setValue("CapModel", amendCapModel);
+		aa.env.setValue("CapModel", amendCapModel);
       
-		// aa.env.setValue("CAP_MODEL_INITED", "TRUE");       
-    aa.env.setValue("CapModel",parentCapId) ;
+		aa.env.setValue("CAP_MODEL_INITED", "TRUE");       
     }
     catch(e)
     { 
@@ -1093,5 +1186,6 @@ function getCapDetailByID(capId)
   // Return capDetailScriptModel
   return capDetailScriptModel;
 }
+
 
 
