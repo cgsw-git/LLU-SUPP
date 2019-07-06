1fe7ab2044360dada33ee029402207d4236cbfee
lluaca <44002415+lluaca@users.noreply.github.com>
2019-07-06 12:55:01 -0700

    Update REBUILD_CUSTOM_LISTS.js
    added logging

--- Scripts/REBUILD_CUSTOM_LISTS.js
+++ Scripts/REBUILD_CUSTOM_LISTS.js
@@ -178,9 +178,13 @@ if (getResult.getSuccess()) {
       customId = capId.getCustomID();
       //// logDebug(capId  + " " + customId + " " + capName);
       
-      // if (customId != "FA0000868") {continue ;}
-      if ( list[i].getCapStatus() != "CAP Required" ) { continue; }
-      logDebug("record id: " + customId );
+      // if (customId != "FA0000868") {
+      if ( list[i].getCapStatus() != "CAP Required" ) { 
+        // logDebug("skipping; " + customId);
+        continue; 
+      }else{
+        logDebug("processing record id: " + customId );
+      }
 
       // logDebug("remove the custom list");
       removeASITable("CAP", capId);
