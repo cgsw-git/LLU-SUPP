55d2de852d7f5d8c0939e8c953f4a69c17cee9ea
lluaca <44002415+lluaca@users.noreply.github.com>
2019-09-10 11:23:48 -0700

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -1,12 +1,9 @@
 // Generate and attach the 5001 Inspection Report to the inspected record
-inspObj = aa.inspection.getInspection(capId,inspId).getOutput();
-var inspDate =  inspObj.getInspectionDate().getMonth() + "/" + inspObj.getInspectionDate().getDayOfMonth() + "/" + inspObj.getInspectionDate().getYear();
-var inspType = inspObj.getInspectionType();
-var customID = capId.getCustomID();
-var reportParams = aa.util.newHashMap();
-addParameter(reportParams,"departmentID",customID);
-addParameter(reportParams, "inspectionDate",inspDate);
-addParameter(reportParams, "inspectionType",inspType);
+
+// support for new 5001 inspection report with only inspection id parameter
+
+addParameter(reportParams,"g6ActNum",inspId);
 var myResult = runReportAttach(capId,"5001 Inspection Report",reportParams);
+
 // End generate and attach the 5001 Inspection Report to the inspected record
 
