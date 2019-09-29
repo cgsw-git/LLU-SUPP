1c5bc1583dba66ebfcf99462975803cbb558af87
lluaca <44002415+lluaca@users.noreply.github.com>
2019-09-28 17:20:12 -0700

    Update CTRCA;ENVHEALTH!~!~!DEPARTMENTAL FIRE DRILL.js

--- Scripts/CTRCA;ENVHEALTH!~!~!DEPARTMENTAL FIRE DRILL.js
+++ Scripts/CTRCA;ENVHEALTH!~!~!DEPARTMENTAL FIRE DRILL.js
@@ -23,7 +23,7 @@ This event script:
 /* CTRCA */  //var eventName = "ConvertToRealCAPAfter";
 
 // var useProductScript = false;  // set to true to use the "productized" master scripts (events->master scripts), false to use scripts from (events->scripts)
-// var runEvent = true; // set to true to simulate the event and run all std choices/scripts for the record type.  
+// var runEvent = false; // set to true to simulate the event and run all std choices/scripts for the record type.  
 
 /* master script code don't touch */ 
 // aa.env.setValue("EventName",eventName); var vEventName = eventName;  var controlString = eventName;  var tmpID = aa.cap.getCapID(myCapId).getOutput(); if(tmpID != null){aa.env.setValue("PermitId1",tmpID.getID1()); 	aa.env.setValue("PermitId2",tmpID.getID2()); 	aa.env.setValue("PermitId3",tmpID.getID3());} aa.env.setValue("CurrentUserID",myUserId); var preExecute = "PreExecuteForAfterEvents";var documentOnly = false;var SCRIPT_VERSION = 3.0;var useSA = false;var SA = null;var SAScript = null;var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 	useSA = true; 		SA = bzr.getOutput().getDescription();	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }	}if (SA) {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",SA,useProductScript));	/* force for script test*/ showDebug = true; eval(getScriptText(SAScript,SA,useProductScript));	}else {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useProductScript));	}	eval(getScriptText("INCLUDES_CUSTOM",null,useProductScript));if (documentOnly) {	doStandardChoiceActions2(controlString,false,0);	aa.env.setValue("ScriptReturnCode", "0");	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");	aa.abortScript();	}var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";var doStdChoices = true;  var doScripts = false;var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;if (bzr) {	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	}	function getScriptText(vScriptName, servProvCode, useProductScripts) {	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();	vScriptName = vScriptName.toUpperCase();	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();	try {		if (useProductScripts) {			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);		} else {			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");		}		return emseScript.getScriptText() + "";	} catch (err) {		return "";	}}logGlobals(AInfo); if (runEvent && typeof(doStandardChoiceActions) == "function" && doStdChoices) try {doStandardChoiceActions(controlString,true,0); } catch (err) { logDebug(err.message) } if (runEvent && typeof(doScriptActions) == "function" && doScripts) doScriptActions(); var z = debug.replace(/<BR>/g,"\r");  aa.print(z); 
@@ -35,7 +35,6 @@ This event script:
 try {
 	showDebug = true;
   var outOfCompliance = false;
-  copyContacts(parentCapId,capId);
   copyAddresses(parentCapId,capId);
   editAppName(aa.cap.getCap(parentCapId).getOutput().specialText, capId)
 	var parentCapIDString = parentCapId.getCustomID();
@@ -123,7 +122,6 @@ try {
   if (outOfCompliance) {
     updateAppStatus("CAP Required","Updated by EMSE Script",parentCapId);
   }
-
   
   // send email notification to contacts
   // Provide the ACA URl - This should be set in INCLUDES_CUSTOM_GLOBALS
@@ -146,19 +144,22 @@ try {
 
   var rptParams = aa.util.newHashMap();
   rptParams.put("recordID", capId.getCustomID());
-  logDebug("report recordID: " + capId.getCustomID() );
+  rptParams.put("recordIDText", capId.getCustomID());
+  logDebug("report recordID parameter: " + capId.getCustomID() );
 
   if(!matches(reportName,null,undefined,"")){
     // Call runReportAttach to attach the report to parent record Documents Tab
-    // declare report with global scope 
-    var myResults = myRunReportAttach(parentCapId,reportName,rptParams);
+    var attachToRecord = parentCapId;
+    logDebug("attach report to record: " + attachToRecord);
+    var myResults = myRunReportAttach(attachToRecord,reportName,rptParams);
     logDebug("Run report attach results: " + myResults.getSuccess());
     if (myResults.getSuccess()) {
       var reportResults = myResults.getOutput();
       // logDebugObject(reportResults);
       var reportName = reportResults.name;
+      logDebug("reportName: " + reportName);
       // var reportName = "5012FireDrillObservation_20190925_161330.pdf";
-      reportModel = getRecordModelByDocumentName(parentCapId, reportName)
+      reportModel = getRecordModelByDocumentName(attachToRecord, reportName)
       if (reportModel) {
         var eParams = getACADocDownloadParam4Notification(eParams,acaURL,reportModel);
         logDebug("getRecordModelByDocumentName reportURL: " + getACADocumentDownloadUrl(acaURL,reportModel));
@@ -182,8 +183,8 @@ try {
       addParameter(eParams, "$$parentAltID$$", parentCapIDString);
       tContactObj.getEmailTemplateParams(eParams);
       getPrimaryAddressLineParam4Notification(eParams);
-      logDebug(capId);
-      logDebug(getACARecordURL(acaURL));
+      // logDebug(capId);
+      // logDebug(getACARecordURL(acaURL));
       if(!matches(reportName,null,undefined,"")){
         // Call runReport4Email to generate the report and send the email
         runReport4Email(capId,reportName,tContactObj,rptParams,eParams,notificationTemplate,cap.getCapModel().getModuleName(),agencyReplyEmail);	
@@ -193,8 +194,6 @@ try {
       }
     }
   }
-  
-  
 } catch (err) {
 	logDebug("A JavaScript Error occured: " + err.message);
 }
@@ -205,7 +204,8 @@ try {
 
 /**
  * Runs a report with any specified parameters and attaches it to the record
- *
+ * *** This special version returns the attach results instead of the reportInfoScript object 
+ *     to enable the caller to have access to attachment properties to create a deep link to the attachment
  * @example
  *		runReportAttach(capId,"ReportName","altid",capId.getCustomID(),"months","12");
  *		runReportAttach(capId,"ReportName",paramHashtable);
@@ -213,7 +213,7 @@ try {
  *			itemCapId - capId object 
  * @param {report parameter pairs} or {hashtable}
  *			optional parameters are report parameter pairs or a parameters hashtable
- * @returns {reportInfoScript object}
+ * @returns {result object}
  *			if the report was generated and attached return true
  *
  */
