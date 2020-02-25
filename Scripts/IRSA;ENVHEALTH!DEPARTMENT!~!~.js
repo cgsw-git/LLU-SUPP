de38dc6fcee55657b5965f3e271a0ce136d3ca20
lluaca <44002415+lluaca@users.noreply.github.com>
2020-02-25 13:45:59 -0800

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js
    Fixed to send inspId.toString() and added the debug report parameter

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -8,9 +8,10 @@ var reportParams = aa.util.newHashMap();
 addParameter(reportParams,"departmentID",customID);
 addParameter(reportParams, "inspectionDate",inspDate);
 addParameter(reportParams, "inspectionType",inspType);
+addParameter(reportParams, "debug","N");
 
 // support for new 5001 inspection report with only inspection id parameter
-addParameter(reportParams,"g6ActNum",inspId);
+addParameter(reportParams,"g6ActNum",inspId.toString());
 
 // generate report
 var myResult = runReportAttach(capId,"5001 Inspection Report",reportParams);
