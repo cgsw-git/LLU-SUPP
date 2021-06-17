0c09b9159bcc9e9395599a00d61d443c64e7ed96
lluaca <44002415+lluaca@users.noreply.github.com>
2021-06-17 16:15:52 -0700

    Update INCLUDES_CUSTOM_GLOBALS.js
    change RPDO to SUPP due to refresh from PROD

--- Scripts/INCLUDES_CUSTOM_GLOBALS.js
+++ Scripts/INCLUDES_CUSTOM_GLOBALS.js
@@ -12,7 +12,7 @@
 |	Ifchanges are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
 var feeEstimate=false;
-var acaURL = "aca-prod.accela.com/LLU";
+var acaURL = "aca-supp.accela.com/LLU";
 var agencyReplyEmail = "LLU.EHS@accela.com";
 var wfComment; 
 if(vEventName.equals("FeeEstimateAfter4ACA")) 
