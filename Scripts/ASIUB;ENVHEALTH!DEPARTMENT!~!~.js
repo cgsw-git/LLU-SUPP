596a4d8390e8737a0c98c102a5a841ee29fd7182
lluaca <44002415+lluaca@users.noreply.github.com>
2021-06-17 17:31:24 -0700

    Update ASIUB;ENVHEALTH!DEPARTMENT!~!~.js
    added more debug information

--- Scripts/ASIUB;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/ASIUB;ENVHEALTH!DEPARTMENT!~!~.js
@@ -70,7 +70,7 @@ try
 				var parentColumnName = parentFieldObject.getFieldLabel();
 				if (parentColumnName == "CAP Status" ){
 						setUpdateColumnValue(updateRowsMap, parentRowID, "CAP Status Before", parentFieldObject.getInputValue());
-						logDebug("CAP Status Before set to " + parentFieldObject.getInputValue());
+						logDebug("CAP Status Before set to " + parentFieldObject.getInputValue() + " on row " + i );
 					}
 			}
 		}else{
