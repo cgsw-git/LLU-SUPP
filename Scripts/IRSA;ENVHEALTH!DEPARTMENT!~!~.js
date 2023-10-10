f38e683bd5ebbfecb98f5db706216ea2754cf7ef
cgsw-git <44002415+cgsw-git@users.noreply.github.com>
2023-10-10 13:26:24 -0700

    Update IRSA;ENVHEALTH!DEPARTMENT!~!~.js

--- Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
+++ Scripts/IRSA;ENVHEALTH!DEPARTMENT!~!~.js
@@ -1,23 +1,24 @@
 /*
 Revision log
 6/30/2023	mz	updated record status to CAP Required when a deficiency is cited
-/*
+9/28/2023	mz	added support for violations cited as Corrected Onsite
+*/
 
 
-// var myCapId = "FA0000868";
-// var myUserId = "ADMIN";
-// var inspId = 17681228;
+var myCapId = "FA0002019";
+var myUserId = "ADMIN";
+var inspId = 18300083;
 
 /* ASA  */  //var eventName = "ApplicationSubmitAfter";
 /* WTUA */  //var eventName = "WorkflowTaskUpdateAfter";  wfTask = "Application Submittal";	  wfStatus = "Admin Approved";  wfDateMMDDYYYY = "01/27/2015";
 /* IRSA */  //var eventName = "InspectionResultSubmitAfter" ; inspResult = "Failed"; inspResultComment = "Comment";  inspType = "Roofing"
 /* ISA  */  //var eventName = "InspectionScheduleAfter" ; inspType = "Roofing"
-/* PRA  */  //var eventName = "PaymentReceiveAfter";  
+/* PRA  */  var eventName = "PaymentReceiveAfter";  
 
-// var useProductScript = false;  // set to true to use the "productized" master scripts (events->master scripts), false to use scripts from (events->scripts)
-// var runEvent = false; // set to true to simulate the event and run all std choices/scripts for the record type.  
+var useProductScript = false;  // set to true to use the "productized" master scripts (events->master scripts), false to use scripts from (events->scripts)
+var runEvent = false; // set to true to simulate the event and run all std choices/scripts for the record type.  
 
-// /* master script code don't touch */ aa.env.setValue("EventName",eventName); var vEventName = eventName;  var controlString = eventName;  var tmpID = aa.cap.getCapID(myCapId).getOutput(); if(tmpID != null){aa.env.setValue("PermitId1",tmpID.getID1()); 	aa.env.setValue("PermitId2",tmpID.getID2()); 	aa.env.setValue("PermitId3",tmpID.getID3());} aa.env.setValue("CurrentUserID",myUserId); var preExecute = "PreExecuteForAfterEvents";var documentOnly = false;var SCRIPT_VERSION = 3.0;var useSA = false;var SA = null;var SAScript = null;var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 	useSA = true; 		SA = bzr.getOutput().getDescription();	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }	}if (SA) {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",SA,useProductScript));	/* force for script test*/ showDebug = true; eval(getScriptText(SAScript,SA,useProductScript));	}else {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useProductScript));	}	eval(getScriptText("INCLUDES_CUSTOM",null,useProductScript));if (documentOnly) {	doStandardChoiceActions2(controlString,false,0);	aa.env.setValue("ScriptReturnCode", "0");	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");	aa.abortScript();	}var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";var doStdChoices = true;  var doScripts = false;var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;if (bzr) {	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	}	function getScriptText(vScriptName, servProvCode, useProductScripts) {	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();	vScriptName = vScriptName.toUpperCase();	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();	try {		if (useProductScripts) {			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);		} else {			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");		}		return emseScript.getScriptText() + "";	} catch (err) {		return "";	}}logGlobals(AInfo); if (runEvent && typeof(doStandardChoiceActions) == "function" && doStdChoices) try {doStandardChoiceActions(controlString,true,0); } catch (err) { logDebug(err.message) } if (runEvent && typeof(doScriptActions) == "function" && doScripts) doScriptActions(); var z = debug.replace(/<BR>/g,"\r");  aa.print(z); 
+/* master script code don't touch */ aa.env.setValue("EventName",eventName); var vEventName = eventName;  var controlString = eventName;  var tmpID = aa.cap.getCapID(myCapId).getOutput(); if(tmpID != null){aa.env.setValue("PermitId1",tmpID.getID1()); 	aa.env.setValue("PermitId2",tmpID.getID2()); 	aa.env.setValue("PermitId3",tmpID.getID3());} aa.env.setValue("CurrentUserID",myUserId); var preExecute = "PreExecuteForAfterEvents";var documentOnly = false;var SCRIPT_VERSION = 3.0;var useSA = false;var SA = null;var SAScript = null;var bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_FOR_EMSE"); if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") { 	useSA = true; 		SA = bzr.getOutput().getDescription();	bzr = aa.bizDomain.getBizDomainByValue("MULTI_SERVICE_SETTINGS","SUPER_AGENCY_INCLUDE_SCRIPT"); 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }	}if (SA) {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",SA,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",SA,useProductScript));	/* force for script test*/ showDebug = true; eval(getScriptText(SAScript,SA,useProductScript));	}else {	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useProductScript));	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useProductScript));	}	eval(getScriptText("INCLUDES_CUSTOM",null,useProductScript));if (documentOnly) {	doStandardChoiceActions2(controlString,false,0);	aa.env.setValue("ScriptReturnCode", "0");	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");	aa.abortScript();	}var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";var doStdChoices = true;  var doScripts = false;var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;if (bzr) {	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";	}	function getScriptText(vScriptName, servProvCode, useProductScripts) {	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();	vScriptName = vScriptName.toUpperCase();	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();	try {		if (useProductScripts) {			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);		} else {			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");		}		return emseScript.getScriptText() + "";	} catch (err) {		return "";	}}logGlobals(AInfo); if (runEvent && typeof(doStandardChoiceActions) == "function" && doStdChoices) try {doStandardChoiceActions(controlString,true,0); } catch (err) { logDebug(err.message) } if (runEvent && typeof(doScriptActions) == "function" && doScripts) doScriptActions(); var z = debug.replace(/<BR>/g,"\r");  aa.print(z); 
 
 //
 // User code goes here
@@ -59,7 +60,7 @@ catch (err) {
 
 // end user code
 // aa.env.setValue("ScriptReturnCode", "1"); 	
-// aa.env.setValue("ScriptReturnMessage", debug)
+aa.env.setValue("ScriptReturnMessage", debug)
 
 
 //Custom functions
@@ -98,7 +99,10 @@ function addCorrectiveActionPlanASIT(){
 		  if (gsi.status == "Out of Compliance" || gsi.status == "Major - Out of Compliance"  && gsi.validInfo) {
 			updateAppStatus("CAP Required","Updated by EMSE Script",capId);
 			logDebug("Processing guidesheet item " + x + " with status of " + gsi.status);
-			generateCAPViolationsASITRow(gsi);
+			generateCAPViolationsASITRow(gsi,false); //call generateCAPViolationsASITRow(gsi,cos) with corrected onsite (cos) as false
+		  }else if (gsi.status == "Corrected Onsite"  && gsi.validInfo){
+			logDebug("Processing guidesheet item " + x + " with status of " + gsi.status);
+			generateCAPViolationsASITRow(gsi,true); //call generateCAPViolationsASITRow(gsi,cos) with corrected onsite (cos) as true
 		  }else{
 			logDebug("Skipping guidesheet item " + x + " with status of " + gsi.status);
 		  }
@@ -110,7 +114,7 @@ function addCorrectiveActionPlanASIT(){
 }  
  
 
-function generateCAPViolationsASITRow(gsi) {
+function generateCAPViolationsASITRow(gsi,cos) {
 	//logDebug("    made it to GenerateCAPviolationsASITRow");
 	//inspObj = aa.inspection.getInspection(capId,inspId).getOutput();
 	//logDebugObject(inspObj);
@@ -132,7 +136,12 @@ function generateCAPViolationsASITRow(gsi) {
 	rowVals.push({colName: 'Vio. Status', colValue: gsi.status});
 	rowVals.push({colName: 'Inspector Comment', colValue: gsi.info["Inspector Comment"]});
 	rowVals.push({colName: 'Inspection Type', colValue: inspType});
-	rowVals.push({colName: 'CAP Status', colValue: "Incomplete"});
+	if (cos) { // if corrected Onsite (cos) then set the cap status to 'Approved' and note in field Corrective Action it was corrected onsite
+		rowVals.push({colName: 'CAP Status', colValue: "Approved"});
+		rowVals.push({colName: 'Corrective Action', colValue: "Corrected Onsite"});
+	}else{
+		rowVals.push({colName: 'CAP Status', colValue: "Incomplete"});
+	}
 	var addrResult = aa.address.getAddressByCapId(capId);
 	if (addrResult) {
 	  var addrArray = new Array();
@@ -176,9 +185,11 @@ function generateCAPViolationsASITRow(gsi) {
 	logDebug("Updating Item Was Processed checkbox");
 	myUpdateGuidesheetASIField(inspId, gsi.gsType,gsi.text,"CKLST_CMNT","ADDITIONAL VIOLATION INFO", "Item Was Processed","CHECKED",capId);
 
-	// set the record status to CAP Required
-	appStatus = "CAP Required"
-	updateAppStatus(appStatus,"Updated by EMSE Script",capId);					
+	// set the record status to CAP Requiredc
+	if ( ! cos ) {
+		appStatus = "CAP Required"
+		updateAppStatus(appStatus,"Updated by EMSE Script",capId);					
+	}
 }
 
 function myUpdateGuidesheetASIField(inspId,gName,gItem,asiGroup,asiSubGroup, asiLabel,newValue,capId) {
